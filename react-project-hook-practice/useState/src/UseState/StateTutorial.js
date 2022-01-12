7c641ba0919875236900232073f7feb4a91d511a
Bram <brammerten@live.nl>
2022-01-12 11:23:33 +0100

    User List Tracker

---
+++ react-project-hook-practice/useState/src/UseState/StateTutorial.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+const StateTutorial = () => {
+  const [counter, setCounter] = useState(2);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div>
+      {counter} <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+export default StateTutorial;
