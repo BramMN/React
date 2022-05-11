4b74b1ed9b0c18cfa4f442a3e4667bd54a36e254
Bram <brammerten@live.nl>
2022-05-11 14:13:38 +0200

    importing, linking, appjs

---
+++ react-next-meetup-page/pages/index.js
@@ -0,0 +1,28 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 5, 123456 some city",
+    description: "First Meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 15, 123456 some city",
+    description: "Second Meetup!",
+  },
+];
+
+function HomePage() {
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  );
+}
+
+export default HomePage;
