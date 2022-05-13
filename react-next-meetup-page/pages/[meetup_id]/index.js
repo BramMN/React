4949342f888bb4cbfc82d074d364c1d591756d52
Bram <brammerten@live.nl>
2022-05-13 12:12:51 +0200

    add mongodb api route

--- react-next-meetup-page/pages/[meetup_id]/index.js
+++ react-next-meetup-page/pages/[meetup_id]/index.js
@@ -12,4 +12,42 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetup_id: "m1",
+        },
+      },
+      {
+        params: {
+          meetup_id: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupid = context.params.meetup_id;
+  console.log(meetupid);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupid,
+        title: "First Meetup",
+        address: "Some Street",
+        description: "First meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
---
+++ react-next-meetup-page/pages/api/new-meetup.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb"
+
+async function handler(req, res) {
+    if (req.method === "POST") {
+        const data = req.body
+
+        const client = await MongoClient.connect("mongodb+srv://testBram:fQ0rUUxY6ZxAQBP5@cluster0.jam5w.mongodb.net/meetups?retryWrites=true&w=majority")
+        const db = client.db()
+
+        const meetupsCollection = db.collection("meetups")
+
+        const result = await meetupsCollection.insertOne(data)
+
+        console.log(result)
+
+        client.close()
+
+        res.status(201).json({message: "Meetup inserted!"})
+    }
+}
+
+export default handler;
