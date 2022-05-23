43d1e0a892814a8436bb886acd13c7c97e097835
Bram <brammerten@live.nl>
2022-05-23 11:10:07 +0200

    add remove handler

--- react-food-ordering-website/src/store/CartProvider.js
+++ react-food-ordering-website/src/store/CartProvider.js
@@ -20,7 +20,7 @@ const cartReducer = (state, action) => {
         amount: existingCartItem.amount + action.item.amount,
       };
       updatedItems = [...state.items];
-      updatedItems[existingCartItem] = updatedItem;
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
       updatedItems = state.items.concat(action.item);
     }
@@ -30,6 +30,27 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    const existingItem = state.item[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingItem.price
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  
   return defaultCartState;
 };
 
