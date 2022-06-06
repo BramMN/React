342bbb454988cb175a8de0af733b4daa0459af9a
Bram <brammerten@live.nl>
2022-06-06 11:04:07 +0200

    post submissions into dbase

--- react-http-requests/src/App.js
+++ react-http-requests/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react"
 
 import MoviesList from "./components/MoviesList"
+import AddMovie from "./components/AddMovie"
 import "./App.css"
 
 function App() {
@@ -8,26 +9,29 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-const fetchMoviesHandler = useCallback(async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
     setError(null)
     try {
-      const response = await fetch("https://swapi.dev/api/films/")
+      const response = await fetch("")
       if (!response.ok) {
         throw new Error("Something went wrong!")
       }
 
       const data = await response.json()
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        }
-      })
-      setMovies(transformedMovies)
+      const loadedMovies = []
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      setMovies(loadedMovies)
     } catch (error) {
       setError(error.message)
     }
@@ -36,7 +40,19 @@ const fetchMoviesHandler = useCallback(async () => {
 
   useEffect(() => {
     fetchMoviesHandler()
-  }, [fetchMoviesHandler]);
+  }, [fetchMoviesHandler])
+
+  async function addMovieHandler(movie) {
+    const response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    const data = await response.json()
+    console.log(data)
+  }
 
   let content = <p>Found no movies.</p>
 
@@ -55,11 +71,12 @@ const fetchMoviesHandler = useCallback(async () => {
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   )
 }
