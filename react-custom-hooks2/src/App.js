c07c5df9ee3aef9d9c19257030897b84aa513870
Bram <brammerten@live.nl>
2022-06-07 11:16:48 +0200

    implement custom hook in app js

--- react-custom-hooks2/src/App.js
+++ react-custom-hooks2/src/App.js
@@ -1,61 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react"
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks"
+import NewTask from "./components/NewTask/NewTask"
+import useHttp from "./hooks/use-http"
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([])
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        ''
-      );
-
-      console.log(response)
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp()
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = taskObj => {
+      const loadedTasks = []
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text })
+      }
+      setTasks(loadedTasks)
+    }
+    
+    fetchTasks({ url: "" }, transformTasks)
+  }, [fetchTasks])
 
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
+  const taskAddHandler = task => {
+    setTasks(prevTasks => prevTasks.concat(task))
+  }
 
   return (
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
+      <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
