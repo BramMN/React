12cb17eaf627f84268b99c2b06204a922d1318f7
Bram <brammerten@live.nl>
2022-06-08 10:47:20 +0200

    change structure to custom hook

--- react-user-input/src/components/SimpleInput.js
+++ react-user-input/src/components/SimpleInput.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react"
+import useInput from "../hooks/use-input"
 
 const SimpleInput = props => {
-  const [enteredName, setEnteredName] = useState("")
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
-  const [enteredEmail, setEnteredEmail] = useState("")
-  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false)
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangedHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput
+  } = useInput(value => value.trim() !== "")
 
-  const enteredNameIsValid = enteredName.trim() !== ""
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
-
-  const enteredEmailIsValid = enteredEmail.includes("@") && enteredEmail.trim().length > 6
-  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched
+    const {
+      value: enteredEmail,
+      isValid: enteredEmailIsValid,
+      hasError: emailInputHasError,
+      valueChangeHandler: emailChangedHandler,
+      inputBlurHandler: emailBlurHandler,
+      reset: resetEmailInput
+    } = useInput(value => value.includes("@") && value.trim().length > 6)
 
   let formIsValid = false
 
@@ -18,53 +25,32 @@ const SimpleInput = props => {
     formIsValid = true
   }
 
-  const nameInputChangHandler = event => {
-    setEnteredName(event.target.value)
-  }
-
-  const emailInputChangHandler = event => {
-    setEnteredEmail(event.target.value)
-  }
-
-  const nameInputBlurHandler = event => {
-    setEnteredNameTouched(true)
-  }
-
-  const emailInputBlurHandler = event => {
-    setEnteredEmailTouched(true)
-  }
-
   const formSubmissionHandler = event => {
     event.preventDefault()
-
-    setEnteredNameTouched(true)
-    setEnteredEmailTouched(true)
 
     if (!enteredNameIsValid || !enteredEmailIsValid) {
       return
     }
 
     console.log(enteredName, enteredEmail)
-    setEnteredName("")
-    setEnteredNameTouched(false)
-    setEnteredEmail("")
-    setEnteredEmailTouched(false)
+    resetNameInput()
+    resetEmailInput()
   }
 
-  const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control"
-  const emailInputClasses = emailInputIsInvalid ? "form-control invalid" : "form-control"
+  const nameInputClasses = nameInputHasError ? "form-control invalid" : "form-control"
+  const emailInputClasses = emailInputHasError ? "form-control invalid" : "form-control"
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangHandler} onBlur={nameInputBlurHandler} value={enteredName} />
-        {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
+        <input type='text' id='name' onChange={nameChangedHandler} onBlur={nameBlurHandler} value={enteredName} />
+        {nameInputHasError && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className={emailInputClasses}>
         <label htmlFor='name'>Your E-mail</label>
-        <input type='email' id='email' onChange={emailInputChangHandler} onBlur={emailInputBlurHandler} value={enteredEmail} />
-        {emailInputIsInvalid && <p className='error-text'>Email must contain '@' and be length 6.</p>}
+        <input type='email' id='email' onChange={emailChangedHandler} onBlur={emailBlurHandler} value={enteredEmail} />
+        {emailInputHasError && <p className='error-text'>Email must contain '@' and be length 6.</p>}
       </div>
       <div className='form-actions'>
         <button disabled={!formIsValid}>Submit</button>
