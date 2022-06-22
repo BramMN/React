0da3f5a4536958624ebdd14931428e1ed947e550
Bram <brammerten@live.nl>
2022-06-22 11:01:37 +0200

    nested routes, page setup

--- react-routing-project/src/pages/AllQuotes.js
+++ react-routing-project/src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList"
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Bram", text: "Learning React is fun!" },
+  { id: "q2", author: "Job", text: "Learning React is great!" },
+]
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>
+  return <QuoteList quotes={DUMMY_QUOTES} />
 }
 
 export default AllQuotes
