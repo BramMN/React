6cd2d88871de840baec616f535caf3f401b1fc64
BramMN <Brammerten@live.nl>
2022-08-13 11:56:35 +0200

    using custom store

--- react-replace-redux/src/components/Products/ProductItem.js
+++ react-replace-redux/src/components/Products/ProductItem.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import { useContext } from 'react';
-import { ProductsContext } from '../../context/products-context';
+import React from "react"
+import { useStore } from "../../hooks-store/store"
 
-import Card from '../UI/Card';
-import './ProductItem.css';
+import Card from "../UI/Card"
+import "./ProductItem.css"
 
 const ProductItem = props => {
-  const toggleFav = useContext(ProductsContext).toggleFav
+  const dispatch = useStore()[1]
 
   const toggleFavHandler = () => {
-    toggleFav(props.id)
-  };
+    dispatch("TOGGLE_FAV", props.id)
+  }
 
   return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+    <Card style={{ marginBottom: "1rem" }}>
+      <div className='product-item'>
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
         <p>{props.description}</p>
-        <button
-          className={!props.isFav ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
-        >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+        <button className={!props.isFav ? "button-outline" : ""} onClick={toggleFavHandler}>
+          {props.isFav ? "Un-Favorite" : "Favorite"}
         </button>
       </div>
     </Card>
-  );
-};
+  )
+}
 
-export default ProductItem;
+export default ProductItem
--- react-replace-redux/src/containers/Favorites.js
+++ react-replace-redux/src/containers/Favorites.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import { useContext } from 'react';
+import React from "react"
 
-import FavoriteItem from '../components/Favorites/FavoriteItem';
-import { ProductsContext } from '../context/products-context';
-import './Products.css';
+import FavoriteItem from "../components/Favorites/FavoriteItem"
+import { useStore } from "../hooks-store/store"
+import "./Products.css"
 
 const Favorites = props => {
-  const favoriteProducts = useContext(ProductsContext).products.filter(p => p.isFavorite);
-  let content = <p className="placeholder">Got no favorites yet!</p>;
+  const state = useStore()[0]
+  const favoriteProducts = state.products.filter(p => p.isFavorite)
+  let content = <p className='placeholder'>Got no favorites yet!</p>
   if (favoriteProducts.length > 0) {
     content = (
-      <ul className="products-list">
+      <ul className='products-list'>
         {favoriteProducts.map(prod => (
-          <FavoriteItem
-            key={prod.id}
-            id={prod.id}
-            title={prod.title}
-            description={prod.description}
-          />
+          <FavoriteItem key={prod.id} id={prod.id} title={prod.title} description={prod.description} />
         ))}
       </ul>
-    );
+    )
   }
-  return content;
-};
+  return content
+}
 
-export default Favorites;
+export default Favorites
