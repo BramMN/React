4a6635e3b1c8473f8d67c030852c2dbf8ac3da49
BramMN <Brammerten@live.nl>
2022-08-14 16:09:32 +0200

    writing first custom tests

---
+++ react-unit-tests/src/components/Greeting.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import Greeting from "./Greeting"
+
+describe("Greeting Component", () => {
+  test("Renders Hello World as a text", () => {
+    //Arrange
+    render(<Greeting />)
+
+    //Act
+    //... nothing
+
+    //Assert
+    const helloWorldElement = screen.getByText("Hello World")
+    expect(helloWorldElement).toBeInTheDocument()
+  })
+
+  test("Renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />)
+
+    const outputElement = screen.getByText("good to see you", { exact: false })
+    expect(outputElement).toBeInTheDocument()
+  })
+
+  test("Renders Changed! if the button was clicked", () => {
+    render(<Greeting />)
+
+    const buttonElement = screen.getByRole("button")
+    userEvent.click(buttonElement)
+
+    const outputElement = screen.getByText("Changed!")
+    expect(outputElement).toBeInTheDocument()
+  })
+
+  test("does not render good to see you if the button was clicked", () => {
+    render(<Greeting />)
+
+    const buttonElement = screen.getByRole("button")
+    userEvent.click(buttonElement)
+
+    const outputElement = screen.queryByText("good to see you!", { exact: false })
+    expect(outputElement).toBeNull()
+  })
+})
