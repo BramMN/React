b5ddac1c25617736409e368b4e038cc1c990fdb6
BramMN <Brammerten@live.nl>
2022-09-13 23:10:33 +0200

    change exact status

--- react-unit-tests/src/components/Greeting.test.js
+++ react-unit-tests/src/components/Greeting.test.js
@@ -38,7 +38,7 @@ describe("Greeting Component", () => {
     const buttonElement = screen.getByRole("button")
     userEvent.click(buttonElement)
 
-    const outputElement = screen.queryByText("good to see you!", { exact: false })
+    const outputElement = screen.queryByText("good to see you!")
     expect(outputElement).toBeNull()
   })
 })
