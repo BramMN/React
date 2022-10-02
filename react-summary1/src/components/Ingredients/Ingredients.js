2408078c10439cb117798bb0354f3309aca622fc
BramMN <Brammerten@live.nl>
2022-10-02 22:26:29 +0200

    add filter functionality

--- react-summary1/src/components/Ingredients/Ingredients.js
+++ react-summary1/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 
 import IngredientForm from "./IngredientForm"
 import Search from "./Search"
@@ -8,23 +8,14 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([])
 
   useEffect(() => {
-    fetch(process.env.REACT_APP_API_KEY)
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = []
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          })
-        }
-        setUserIngredients(loadedIngredients)
-      })
+    console.log("render")
+  })
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients)
   }, [])
 
   const addIngredientHandler = ingredient => {
-    console.log(process.env.REACT_APP_API_KEY)
     fetch(process.env.REACT_APP_API_KEY, {
       method: "POST",
       body: JSON.stringify(ingredient),
@@ -47,7 +38,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
