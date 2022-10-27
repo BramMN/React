7cc0472c5d410579675dc54d2fa0ed4d910e7b73
BramMN <Brammerten@live.nl>
2022-10-27 13:09:24 +0200

    introducing usestate

--- react-summary2/src/components/Todo.js
+++ react-summary2/src/components/Todo.js
@@ -1,14 +1,24 @@
+import { useState } from "react"
+import Backdrop from "./Backdrop"
+import Modal from "./Modal"
+
 const Todo = props => {
-  function deleteHandler () {
-    console.log("click")
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  function deleteHandler() {
+    setModalIsOpen(true)
   }
-  
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   )
 }
