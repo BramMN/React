fe8e0d07ed3b5db3e3c1f7acdf92b2072525b247
BramMN <Brammerten@live.nl>
2022-10-31 10:24:46 +0100

    adding a backend, post request

--- react-summary2-meetup/src/pages/NewMeetup.js
+++ react-summary2-meetup/src/pages/NewMeetup.js
@@ -1,10 +1,18 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(process.env.REACT_APP_API_KEY + "/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: { "Content-Type": "application/json" },
+    })
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
