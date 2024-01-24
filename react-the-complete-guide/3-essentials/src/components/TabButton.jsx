7280d3ce79e010143641ed10dae895d77e0822c1
BramMN <Brammerten@live.nl>
2024-01-24 23:41:51 +0100

    49 - Reacting to Events

--- react-the-complete-guide/3-essentials/src/components/TabButton.jsx
+++ react-the-complete-guide/3-essentials/src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 export function TabButton({ children }) {
+  function handleClick() {
+    console.log("clicked")
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   )
 }
