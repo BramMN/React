2ddb0b10b1543550cb1192ab4046709080939fbb
BramMN <Brammerten@live.nl>
2024-01-24 23:46:14 +0100

    50 - Passing Function as Values to Props

--- react-the-complete-guide/3-essentials/src/components/TabButton.jsx
+++ react-the-complete-guide/3-essentials/src/components/TabButton.jsx
@@ -1,11 +1,7 @@
-export function TabButton({ children }) {
-  function handleClick() {
-    console.log("clicked")
-  }
-
+export function TabButton({ children, onSelect }) {
   return (
     <li>
-      <button onClick={handleClick}>{children}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   )
 }
