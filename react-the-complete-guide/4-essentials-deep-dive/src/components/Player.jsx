06fc4c6aabed0f3865ec923e7e86d43b3ae1b36f
BramMN <Brammerten@live.nl>
2024-01-27 12:53:07 +0100

    74 - Concept repetition: working with state

--- react-the-complete-guide/4-essentials-deep-dive/src/components/Player.jsx
+++ react-the-complete-guide/4-essentials-deep-dive/src/components/Player.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
+
 export function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false)
+
+  const handleEdit = () => {
+    setIsEditing(isEditing => !isEditing)
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input type="text" required /> : <span className="player-name">{name}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   )
 }
