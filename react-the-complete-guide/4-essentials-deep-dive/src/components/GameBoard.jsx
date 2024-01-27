5e5fc1755b39591794d6d4fc0e66d39d2b713a42
BramMN <Brammerten@live.nl>
2024-01-27 13:29:27 +0100

    81 - Lifting State Up

--- react-the-complete-guide/4-essentials-deep-dive/src/components/GameBoard.jsx
+++ react-the-complete-guide/4-essentials-deep-dive/src/components/GameBoard.jsx
@@ -6,15 +6,17 @@ const initialGameBoard = [
   [null, null, null],
 ]
 
-export function GameBoard() {
+export function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
   function handleCellClick(rowIndex, cellIndex) {
     setGameBoard(prevGameBoard => {
       const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-      updatedBoard[rowIndex][cellIndex] = "X"
+      updatedBoard[rowIndex][cellIndex] = activePlayerSymbol
       return updatedBoard
     })
+
+    onSelectSquare()
   }
 
   return (
