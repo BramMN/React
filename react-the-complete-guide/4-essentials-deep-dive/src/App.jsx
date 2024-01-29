ae562b3de5fa0567d5e0bd1b25bc510dc80988bc
BramMN <Brammerten@live.nl>
2024-01-29 12:36:22 +0100

    90 - Lifting computed state up

--- react-the-complete-guide/4-essentials-deep-dive/src/App.jsx
+++ react-the-complete-guide/4-essentials-deep-dive/src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from "react"
 import { GameBoard } from "./components/GameBoard"
 import { Player } from "./components/Player"
 import { Log } from "./components/Log"
+import { WINNING_COMBINATIONS } from "./winning-combinations"
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
 
 function deriveActivePlayer(gameTurns) {
   let activePlayer = "X"
@@ -15,7 +22,21 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([])
+
   const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = initialGameBoard
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn
+    const { row, col } = square
+
+    gameBoard[row][col] = player
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -46,7 +67,7 @@ function App() {
         </ol>
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
+          board={gameBoard}
         />
       </div>
       <Log gameTurns={gameTurns} />
