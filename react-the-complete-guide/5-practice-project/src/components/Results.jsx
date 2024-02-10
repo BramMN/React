d53510355344bc7e323452924054f4f8cdd3670d
BramMN <Brammerten@live.nl>
2024-02-10 23:08:07 +0100

    102 - Outputting results in a list and deriving values

--- react-the-complete-guide/5-practice-project/src/components/Results.jsx
+++ react-the-complete-guide/5-practice-project/src/components/Results.jsx
@@ -1,7 +1,36 @@
-import { calculateInvestmentResults } from "../util/investment"
+import { calculateInvestmentResults, formatter } from "../util/investment"
 
 export function Results({ input }) {
   const results = calculateInvestmentResults(input)
-  console.log(results)
-  return <p>Results...</p>
+  const initialInvestment = results[0].valueEndOfYear - results[0].annualInvestment - results[0].interest
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((result, index) => {
+          const totalInterest = result.valueEndOfYear - result.annualInvestment * result.year - initialInvestment
+          const totalInvested = result.valueEndOfYear - totalInterest
+
+          return (
+            <tr key={index}>
+              <td>{result.year}</td>
+              <td>{formatter.format(result.valueEndOfYear)}</td>
+              <td>{formatter.format(result.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalInvested)}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
