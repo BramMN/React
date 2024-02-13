f0ab213e6e0cd84820ceb0a1f9b7d0e1d759ffa4
BramMN <Brammerten@live.nl>
2024-02-13 18:27:08 +0100

    142. Sharing State Across Components

--- react-the-complete-guide/8-working-with-refs-and-portals/src/components/TimerChallenge.jsx
+++ react-the-complete-guide/8-working-with-refs-and-portals/src/components/TimerChallenge.jsx
@@ -12,6 +12,9 @@ export function TimerChallenge({ title, targetTime }) {
   if (timeRemaining <= 0) {
     clearInterval(timer.current)
     dialog.current.open()
+  }
+
+  function handleReset() {
     setTimeRemaining(targetTime * 1000)
   }
 
@@ -29,9 +32,10 @@ export function TimerChallenge({ title, targetTime }) {
   return (
     <>
       <ResultModal
-        result="lost"
         targetTime={targetTime}
         ref={dialog}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
       />
       <section className="challenge">
         <h2>{title}</h2>
