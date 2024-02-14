a1b1878a4024f043aad4d117830d7da517b88bb9
BramMN <Brammerten@live.nl>
2024-02-14 23:47:35 +0100

    147. Adding a "Projects Sidebar" Component

---
+++ react-the-complete-guide/9-project-management-app/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  )
+}
