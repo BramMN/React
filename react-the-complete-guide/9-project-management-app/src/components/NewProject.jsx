9882b04cdbc0e86afacb32fd1764b6d899a545d5
BramMN <Brammerten@live.nl>
2024-02-16 00:09:58 +0100

    153. Collecting User Input with Refs & Forwarded Refs

--- react-the-complete-guide/9-project-management-app/src/components/NewProject.jsx
+++ react-the-complete-guide/9-project-management-app/src/components/NewProject.jsx
@@ -1,6 +1,23 @@
+import { useRef } from "react"
 import { Input } from "./Input"
 
-export function NewProject() {
+export function NewProject({ onAdd }) {
+  const title = useRef()
+  const description = useRef()
+  const dueDate = useRef()
+
+  function handleSave() {
+    const enteredTitle = title.current.value
+    const enteredDescription = description.current.value
+    const enteredDueDate = dueDate.current.value
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    })
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -8,16 +25,30 @@ export function NewProject() {
           <button className="text-stone-800 hover:text-stone-950">Cancel</button>
         </li>
         <li>
-          <button className="bg-stone-800 text-stone-50 px-6 py-2 rounded-md hover:bg-stone-950">Save</button>
+          <button
+            className="bg-stone-800 text-stone-50 px-6 py-2 rounded-md hover:bg-stone-950"
+            onClick={handleSave}
+          >
+            Save
+          </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
         <Input
+          ref={title}
+          label="Title"
+          type="text"
+        />
+        <Input
+          ref={description}
           label="Description"
           textarea
         />
-        <Input label="Due Date" />
+        <Input
+          ref={dueDate}
+          label="Due Date"
+          type="date"
+        />
       </div>
     </div>
   )
