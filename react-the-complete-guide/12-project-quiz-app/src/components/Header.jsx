dd5fbc354add59df1d362c38a488e86347172ac5
BramMN <Brammerten@live.nl>
2024-02-18 13:09:36 +0100

    192. A First Component & Some State

---
+++ react-the-complete-guide/12-project-quiz-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logoImg from "../assets/quiz-logo.png"
+
+export function Header() {
+  return (
+    <header>
+      <img
+        src={logoImg}
+        alt="Quiz logo"
+      />
+      <h1>ReactQuiz</h1>
+    </header>
+  )
+}
