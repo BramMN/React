f01a665aa6c71981680630d011bf2cc0c941bb19
BramMN <Brammerten@live.nl>
2024-02-18 22:28:29 +0100

    201. Setting Different Timers Based On The Selected Answer

--- react-the-complete-guide/12-project-quiz-app/src/components/QuestionTimer.jsx
+++ react-the-complete-guide/12-project-quiz-app/src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export function QuestionTimer({ timeout, onTimeout }) {
+export function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout)
 
   useEffect(() => {
@@ -26,6 +26,7 @@ export function QuestionTimer({ timeout, onTimeout }) {
       id="question-time"
       max={timeout}
       value={remainingTime}
+      className={mode}
     />
   )
 }
