9d6f3048005d83104789d6823262341eb64e2c92
BramMN <Brammerten@live.nl>
2024-02-18 22:21:33 +0100

    200. Moving Logic To Components That Actually Need It ("Moving State Down")

--- react-the-complete-guide/12-project-quiz-app/src/components/Quiz.jsx
+++ react-the-complete-guide/12-project-quiz-app/src/components/Quiz.jsx
@@ -5,31 +5,14 @@ import quizCompleteImg from "../assets/quiz-complete.png"
 import { Question } from "./Question"
 
 export function Quiz() {
-  const [answerState, setAnswerState] = useState("")
   const [userAnswers, setUserAnswers] = useState([])
 
-  const activeQuestionIndex = answerState === "" ? userAnswers.length : userAnswers.length - 1
+  const activeQuestionIndex = userAnswers.length
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered")
-      setUserAnswers(prevUserAnswers => [...prevUserAnswers, selectedAnswer])
-
-      setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct")
-        } else {
-          setAnswerState("wrong")
-        }
-
-        setTimeout(() => {
-          setAnswerState("")
-        }, 2000)
-      }, 1000)
-    },
-    [activeQuestionIndex]
-  )
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers(prevUserAnswers => [...prevUserAnswers, selectedAnswer])
+  }, [])
 
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
@@ -49,10 +32,7 @@ export function Quiz() {
     <div id="quiz">
       <Question
         key={activeQuestionIndex}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        index={activeQuestionIndex}
         onSelectAnswer={handleSelectAnswer}
         onSkipAnswer={handleSkipAnswer}
       />
