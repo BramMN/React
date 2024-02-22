ecb448f61546e62c85b809aefaa6df20c143423d
BramMN <Brammerten@live.nl>
2024-02-22 22:50:03 +0100

    230. How NOT To Send HTTP Requests (And Why It's Wrong)

--- react-the-complete-guide/15-sending-http-requests/src/components/AvailablePlaces.jsx
+++ react-the-complete-guide/15-sending-http-requests/src/components/AvailablePlaces.jsx
@@ -3,7 +3,13 @@ import Places from "./Places.jsx"
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([])
-  
+
+  fetch("http://localhost:3000/places")
+    .then(response => response.json())
+    .then(data => {
+      setAvailablePlaces(data.places)
+    })
+
   return (
     <Places
       title="Available Places"
