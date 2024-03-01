29eb503a3a0c19f7715ecaf9af260b88092d4341
BramMN <brammtn@gmail.com>
2024-03-01 23:42:56 +0400

    252. Getting User Input via Refs

--- react-the-complete-guide/17-forms-user-input/src/components/StateLogin.jsx
+++ react-the-complete-guide/17-forms-user-input/src/components/StateLogin.jsx
@@ -1,17 +1,30 @@
-import { useRef } from "react"
+import { useState } from "react"
 
 export default function Login() {
-  const email = useRef()
-  const password = useRef()
+  // const [enteredEmail, setEnteredEmail] = useState("")
+  // const [enteredPassword, setEnteredPassword] = useState("")
+  const [enteredValue, setEnteredValue] = useState({
+    email: "",
+    password: "",
+  })
 
   function handleSubmit(event) {
     event.preventDefault()
-
-    const enteredEmail = email.current.value
-    const enteredPassword = password.current.value
-
-    console.log(enteredEmail, enteredPassword)
+    console.log(enteredValue)
   }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValue(prevState => {
+      return {
+        ...prevState,
+        [identifier]: value,
+      }
+    })
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value)
+  // }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -24,7 +37,8 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            ref={email}
+            onChange={event => handleInputChange("email", event.target.value)}
+            value={enteredValue.email}
           />
         </div>
 
@@ -34,7 +48,8 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            ref={password}
+            onChange={event => handleInputChange("password", event.target.value)}
+            value={enteredValue.password}
           />
         </div>
       </div>
