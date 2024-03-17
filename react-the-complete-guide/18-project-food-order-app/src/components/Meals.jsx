743119fd3bb9e6e377cbb3195b2b88c9584f44cb
BramMN <brammtn@gmail.com>
2024-03-18 00:33:09 +0900

    279. Handling HTTP Loading & Error States

--- react-the-complete-guide/18-project-food-order-app/src/components/Meals.jsx
+++ react-the-complete-guide/18-project-food-order-app/src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useHttp } from "../hooks/useHttp"
+import { Error } from "./Error"
 import { MealItem } from "./MealItem"
 
 const requestConfig = {}
@@ -7,7 +8,16 @@ export function Meals() {
   const { data: loadedMeals, isLoading, error } = useHttp("http://localhost:3000/meals", requestConfig, [])
 
   if (isLoading) {
-    return <p>Fetching meals...</p>
+    return <p className="center">Fetching meals...</p>
+  }
+
+  if (error) {
+    return (
+      <Error
+        title="Failed to fetch meals"
+        message={error}
+      />
+    )
   }
 
   return (
