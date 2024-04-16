57fc017c2f7dd4ae13fdfe09c8941db25274988c
BramMN <brammtn@gmail.com>
2024-04-16 23:44:58 +0900

    325. Adding a Second Route

--- react-the-complete-guide/21-Multipage-SPA-React-Router/src/App.js
+++ react-the-complete-guide/21-Multipage-SPA-React-Router/src/App.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { HomePage } from "./pages/Home"
+import { ProductsPage } from "./pages/Products"
 
-const router = createBrowserRouter([{ path: "/", Component: HomePage }])
+const router = createBrowserRouter([
+  { path: "/", Component: HomePage },
+  { path: "/products", Component: ProductsPage },
+])
 
 function App() {
   return <RouterProvider router={router} />
---
+++ react-the-complete-guide/21-Multipage-SPA-React-Router/src/pages/Products.js
@@ -0,0 +1,3 @@
+export function ProductsPage() {
+  return <h1>The Products Page</h1>
+}
