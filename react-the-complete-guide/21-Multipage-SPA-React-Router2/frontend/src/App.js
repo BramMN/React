1383a92f2a20195fd4b8e03c8c365030dcdbb318
BramMN <brammtn@gmail.com>
2024-04-27 17:49:24 +0900

    338. Time to Practice: Solution

--- react-the-complete-guide/21-Multipage-SPA-React-Router2/frontend/src/App.js
+++ react-the-complete-guide/21-Multipage-SPA-React-Router2/frontend/src/App.js
@@ -20,8 +20,37 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { NewEventPage } from "./pages/NewEvent"
+import { EventDetailPage } from "./pages/EventDetail"
+import { EditEventPage } from "./pages/EditEvent"
+import { EventsPage } from "./pages/Events"
+import { HomePage } from "./pages/Home"
+import { RootLayout } from "./pages/Root"
+import { EventsRoot } from "./pages/EventsRoot"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, Component: HomePage },
+      {
+        path: "events",
+        element: <EventsRoot />,
+        children: [
+          { index: true, Component: EventsPage },
+          { path: "new", Component: NewEventPage },
+          { path: ":eventId", Component: EventDetailPage },
+          { path: ":eventId/edit", Component: EditEventPage },
+        ],
+      },
+    ],
+  },
+])
+
 function App() {
-  return <div></div>
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
